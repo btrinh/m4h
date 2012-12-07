67a4965b7b9c0301f057300ffb409e976ab9a3eb
root <root@raspberrypi.(none)>
2012-12-07 08:16:23 -0700

    init commit

---
+++ plugin.php
@@ -0,0 +1,213 @@
+<?php
+/*
+Plugin Name: M4H
+Plugin URI: http://moveforhunger.org
+Description: Directory management for Move For Hunger
+Version: 1.0
+Author: Brian Trinh
+Author URI: http://briantrinh.com
+Author Email: btrinh3612@gmail.com
+License:
+
+  Copyright 2012 Brian Trinh (btrinh3612@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+
+class M4H {
+	 
+	/*--------------------------------------------*
+	 * Constructor
+	 *--------------------------------------------*/
+	
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 */
+	function __construct() {
+    $this->init_plugin_const(); 
+
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'plugin_textdomain' ) );
+
+		// Register admin styles and scripts
+		add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
+	
+		// Register site styles and scripts
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
+	
+		// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+		register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
+		
+	    /*
+	     * TODO:
+	     * Define the custom functionality for your plugin. The first parameter of the
+	     * add_action/add_filter calls are the hooks into which your code should fire.
+	     *
+	     * The second parameter is the function name located within this class. See the stubs
+	     * later in the file.
+	     *
+	     * For more information: 
+	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+	     */
+	    add_action( 'TODO', array( $this, 'action_method_name' ) );
+	    add_filter( 'TODO', array( $this, 'filter_method_name' ) );
+
+    add_action( 'admin_menu', array( $this, 'm4h_admin_menu') );
+	} // end constructor
+	
+  private function init_plugin_const() {
+    if( !defined('PLUGIN_NAME') ) {
+      define('PLUGIN_NAME', 'm4h'); 
+    }
+    if( !defined('PLUGIN_SLUG') ) {
+      define('PLUGIN_SLUG', 'm4h');    
+    }
+  }
+
+  function m4h_admin_menu() {
+    if( function_exists('add_menu_page') ){
+      //require_once(WP_PLUGIN_DIR . '/' . PLUGIN_SLUG . '/views/admin.php');
+      //add_menu_page( 'M4H Directory', 'M4H Directory', 'administrator', 'm4h-admin', array( $this, 'm4h_view_add_user') );
+      //add_menu_page( 'M4H Directory', 'M4H Directory', 'administrator', 'm4h-admin', array( &$this, 'm4h_view_add_user') );
+      //add_menu_page( 'M4H Directory', 'M4H Directory', 'administrator', 'm4h-admin', '/m4h/views/admin.php' );
+      add_menu_page( 'M4H Directory', 'M4H Directory', 'administrator', 'm4h-admin', 'm4h_view_add_user' );
+      //add_menu_page( 'M4H Directory', 'M4H Directory', 'administrator', 'm4h-admin', array( $this, 'm4h_admin_controller') );
+      //add_menu_page( 'M4H Directory', 'M4H Directory', 'administrator', 'm4h-admin', array( &$this, 'views/admin.php') );
+    }
+  }
+
+  function m4h_admin_controller() {
+  ?>
+  <div class="wrap">
+    <h3>M4H</h3>
+  </div>
+  <?php
+  }
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function activate( $network_wide ) {
+		// TODO:	Define activation functionality here
+	} // end activate
+	
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function deactivate( $network_wide ) {
+		// TODO:	Define deactivation functionality here		
+	} // end deactivate
+	
+	/**
+	 * Fired when the plugin is uninstalled.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function uninstall( $network_wide ) {
+		// TODO:	Define uninstall functionality here		
+	} // end uninstall
+
+	/**
+	 * Loads the plugin text domain for translation
+	 */
+	public function plugin_textdomain() {
+	
+		// TODO: replace "plugin-name-locale" with a unique value for your plugin
+		load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+		
+	} // end plugin_textdomain
+
+	/**
+	 * Registers and enqueues admin-specific styles.
+	 */
+	public function register_admin_styles() {
+	
+		// TODO:	Change 'plugin-name' to the name of your plugin
+		wp_enqueue_style( PLUGIN_NAME . '-admin-styles', plugins_url( PLUGIN_NAME . '/css/admin.css' ) );
+    wp_enqueue_style( PLUGIN_NAME . '-bootstrap', plugins_url( PLUGIN_NAME . '/css/bootstrap.min.css' ) );	
+	} // end register_admin_styles
+
+	/**
+	 * Registers and enqueues admin-specific JavaScript.
+	 */	
+	public function register_admin_scripts() {
+	
+		// TODO:	Change 'plugin-name' to the name of your plugin
+		wp_enqueue_script( PLUGIN_NAME . '-admin-script', plugins_url( PLUGIN_NAME . '/js/admin.js' ) );
+		wp_enqueue_script( PLUGIN_NAME . '-bootstrap', plugins_url( PLUGIN_NAME . '/js/admin.js' ) );
+	
+	} // end register_admin_scripts
+	
+	/**
+	 * Registers and enqueues plugin-specific styles.
+	 */
+	public function register_plugin_styles() {
+	
+		// TODO:	Change 'plugin-name' to the name of your plugin
+		wp_enqueue_style( 'plugin-name-plugin-styles', plugins_url( 'plugin-name/css/display.css' ) );
+	
+	} // end register_plugin_styles
+	
+	/**
+	 * Registers and enqueues plugin-specific scripts.
+	 */
+	public function register_plugin_scripts() {
+	
+		// TODO:	Change 'plugin-name' to the name of your plugin
+		wp_enqueue_script( 'plugin-name-plugin-script', plugins_url( 'plugin-name/js/display.js' ) );
+	
+	} // end register_plugin_scripts
+	
+	/*--------------------------------------------*
+	 * Core Functions
+	 *---------------------------------------------*/
+	
+	/**
+ 	 * NOTE:  Actions are points in the execution of a page or process
+	 *        lifecycle that WordPress fires.
+	 *
+	 *		  WordPress Actions: http://codex.wordpress.org/Plugin_API#Actions
+	 *		  Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
+	 *
+	 */
+	function action_method_name() {
+    	// TODO:	Define your action method here
+	} // end action_method_name
+	
+	/**
+	 * NOTE:  Filters are points of execution in which WordPress modifies data
+	 *        before saving it or sending it to the browser.
+	 *
+	 *		  WordPress Filters: http://codex.wordpress.org/Plugin_API#Filters
+	 *		  Filter Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
+	 *
+	 */
+	function filter_method_name() {
+	    // TODO:	Define your filter method here
+	} // end filter_method_name
+  
+} // end class
+
+// include views
+include_once(WP_PLUGIN_DIR . '/m4h/views/admin.php');
+$plugin_name = new M4H();
