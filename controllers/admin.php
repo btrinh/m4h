31e0e2573fac9ce8e3b566604290764cda4b520f
Brian Trinh <btrinh3612@gmail.com>
2012-12-10 16:25:14 -0700

    progressing to displaying error messages

--- controllers/admin.php
+++ controllers/admin.php
@@ -1,5 +1,7 @@
 <?php
 if ( isset( $_POST['m4h-add-user'] ) ) {
+
+/*
   $fname = sanitize_text_field( $_POST['fname'] ); 
   $lname = sanitize_text_field( $_POST['lname'] ); 
   $email = sanitize_email( $_POST['email'] );
@@ -10,22 +12,23 @@ if ( isset( $_POST['m4h-add-user'] ) ) {
   $city = sanitize_text_field( $_POST['city'] );
   $state = sanitize_text_field( $_POST['state'] );
   $zip = sanitize_text_field( $_POST['zip'] );
+*/
   $data = array (
-    "fname" => $fname,
-    "lname" => $lname,
-    "email" => $email,
-    "phone" => $phone,
-    "website" => $website,
-    "address1" => $address1,
-    "address2" => $address2,
-    "city" => $city,
-    "state" => $state,
-    "zip" => $zip
+    'First Name' => array( $_POST['fname'], true ),
+    'Last Name' => array( $_POST['lname'], true ),
+    'Email' => array( $_POST['email'], true ),
+    'Phone Number' => array( $_POST['phone'], true ),
+    'Website' => array( $_POST['website'], false ),
+    'Address 1' => array( $_POST['address1'], true ),
+    'Address 2' => array( $_POST['address2'], false ),
+    'City' => array( $_POST['city'], true ),
+    'State' => array( $_POST['state'], true ),
+    'Zip' => array( $_POST['zip'], true )
   );
 
-  validate_data($data);
+  $errors = validate_data($data);
 
-  /*
+    /*
   $addrKeys = array( 'address1', 'address2', 'city', 'state', 'zip' );
 
   foreach ( $addrKeys as $addrKey ) {
@@ -53,15 +56,41 @@ if ( isset( $_POST['m4h-add-user'] ) ) {
  *  );
  */
 function validate_data($data) {
+  $Errors = array(
+    'empty' => array(),
+    'validation' => array()
+  );
+
   foreach( $data as $field => $v ) {
-    if ( ( empty( $v ) ) && ( $field !== 'website' && $field !== 'address2' ) ) {
+    if ( empty( $v[0] ) && $v[1] === true ) {
       echo "$field is empty<br/>"; 
-    }
+      array_push($Errors['empty'], $field);
+    } 
   }
-  if( !preg_match( '/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $data['phone'] ) ) {
-    echo "Phone # is incorrect";
+
+  if ( empty( $Errors['empty'] ) ) {
+    if( !preg_match( '/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $data['Phone Number'][0] ) )
+      array_push( $Errors['validation'], 'Phone number is invalid.' ); 
+    if ( !is_email( $data['Email'][0] ) )
+      array_push( $Errors['validation'], 'Email is invalid.' );
+    // General validation
+    foreach( $data as $field => $v ) {
+      if ( strlen( $v[0] ) < 3 && $v[1] && $field !== 'State' )
+        array_push( $Errors['validation'], "$field is too short." );
+    } 
   }
-  //echo "Phone #: " . preg_match( '/\d{3}-\d{3}-\d{4}/', $data['phone'] );
+
+
+
+  /*
+  foreach ( $Errors['validation'] as $error ) {
+    //echo "$error</br>"; 
+    add_action('admin_notice', function() {
+      echo "<div 
+    });
+  }
+  */
+  return $Errors;
 }
 
 ?>
